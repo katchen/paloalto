99e14927d7b195e58fc79df63b9ed2875ae579bf
Kevin Ho <keho98@gmail.com>
2012-02-11 17:43:14 -0800

    Search submit fixed

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 <head>
   <!-- include the Tools -->
   <script src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <?php
   $key = 'AIzaSyA9ja49OotC-4jZzkDuBGVj-OQ5AtFGYbw';
-  $sensor = true;
+  $sensor = "false";
   echo '<script type="text/javascript" '.
     'src="http://maps.googleapis.com/maps/api/js?key='.$key.'&sensor='.
     $sensor.'"> </script>';
-
+  ?>
   <!-- standalone page styling (can be removed) -->
   <!-- <link rel="stylesheet" type="text/css" href="http://static.flowplayer.org/tools/css/standalone.css"/>  -->
   <link rel="stylesheet" type="text/css" href="http://static.flowplayer.org/tools/css/overlay-apple.css"/>
@@ -28,45 +29,7 @@
     overflow-y:auto;
   }
   </style>
-        <script type="text/javascript">
-  function showAddress(address) {
-    var latlng = null;
-    var geocoder = new GClientGeocoder();
-    geocoder.getLatLng(
-      address,
-      function(point) {
-        if (!point) {
-          alert(address + " not found");
-        } else {
-          latlng = point;
-          //map.setCenter(point, 13);
-          //var marker = new GMarker(point);
-          //map.addOverlay(marker);
 
-          // As this is user-generated content, we display it as
-          // text rather than HTML to reduce XSS vulnerabilities.
-          //marker.openInfoWindow(document.createTextNode(address));
-        }
-      }
-    );
-    return latlng;
-  }
-  function getMap(addressForm) {
-    var address = addressForm.address;
-    var latlng_obj = showAddress( address);
-    if(!latlng_obj){
-      return;
-    }
-    var options = {
-      //center: new google.maps.LatLng(-34.397, 150.644),
-      center: latlng_obj,
-      zoom: 14, // zoom way in for streetview
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = 
-      new google.maps.Map(document.getElementById("map_canvas"), options);
-  }
-</script>
 </head>
 <body>
 
@@ -79,8 +42,8 @@
 
 
   <form>
-    Enter an address: <input type="text" name="address" /><br />
-    <input type="submit" value="Submit" onclick="getMap(this.form)" />
+    Enter an address: <input id="address" type="text" name="address" /><br />
+    <input id="submit" type="button" value="Submit" onclick="alert("Hello");" />
   </form> 
   <div id="map_canvas" style="width:100%; height:100%"></div>
 
@@ -115,5 +78,47 @@ $(function() {
 
   });
 });
+</script>
+        <script type="text/javascript">
+  function showAddress(address) {
+    var latlng = null;
+    var geocoder = new GClientGeocoder();
+    geocoder.getLatLng(
+      address,
+      function(point) {
+        if (!point) {
+          alert(address + " not found");
+        } else {
+          latlng = point;
+          //map.setCenter(point, 13);
+          //var marker = new GMarker(point);
+          //map.addOverlay(marker);
+
+          // As this is user-generated content, we display it as
+          // text rather than HTML to reduce XSS vulnerabilities.
+          //marker.openInfoWindow(document.createTextNode(address));
+        }
+      }
+    );
+    return latlng;
+  }
+  $('#submit').bind('click', function() {
+    var address = $("#address").text();
+    var latlng_obj = showAddress( address);
+    if(!latlng_obj){
+      return;
+    }
+    var options = {
+      //center: new google.maps.LatLng(-34.397, 150.644),
+      center: latlng_obj,
+      zoom: 14, // zoom way in for streetview
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = 
+      new google.maps.Map(document.getElementById("map_canvas"), options);
+  });
+  function getMap() {
+ 
+  }
 </script>
 </html>
