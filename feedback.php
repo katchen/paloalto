de02f0de0d23aa15d89a5550fa00426fe693e4ca
Kevin Ho <keho98@gmail.com>
2012-02-12 01:13:22 -0800

    Added feedback form

---
+++ feedback.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <form>
+            Name</br>
+            <input type="text"/></br>
+            Email</br>
+            <input type="email"/></br>
+            Comments</br>
+            <textarea rows="5" cols="20"></textarea></br>
+            <input type="submit"/></br>
+        </form>
+    </body>
+</html>
