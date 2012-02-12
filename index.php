68cbc9cd6c81e65b5003ba6e106dc6ba5a66febf
Frank Wang <frankw@cs.stanford.edu>
2012-02-11 22:43:23 -0800

    top css done

--- index.php
+++ index.php
@@ -11,20 +11,7 @@
     $sensor.'"> </script>';
   ?>
   <link rel="stylesheet" type="text/css" href="http://static.flowplayer.org/tools/css/overlay-apple.css"/>
-  <style>
-  /* use a semi-transparent image for the overlay */
-  #overlay {
-    background-image:url(http://static.flowplayer.org/img/overlay/transparent.png);
-    color:#efefef;
-    height:450px;
-  }
-
-  /* container for external content. uses vertical scrollbar, if needed */
-  div.contentWrap {
-    height:441px;
-    overflow-y:auto;
-  }
-  </style>
+  <link rel="stylesheet" type="text/css" href="style.css"/>
   
   <script>
   var geocoder;
@@ -61,17 +48,18 @@
 
   <!-- external page is given in the href attribute (as it should be) -->
   <div id="navbar">
-  <ul>
-  <li> <a href="about.php" rel="#overlay" style="text-decoration:none"> About </a> </li>
-  <li> <a href="admin.php" rel="#overlay" style="text-decoration:none"> Admin </a> </li>
-  <li> <a href="feedback.php" rel="#overlay" style="text-decoration:none"> Feedback </a> </li>
+  <img src="images/header.gif" id="logo"/>
+  <ul id="navbarlinks">
+  <li> <a href="about.php" rel="#overlay"> About </a> </li>
+  <li> <a href="feedback.php" rel="#overlay"> Feedback </a> </li>
+  <li> <a href="admin.php"> Admin </a> </li>
   </ul>
   </div>
   
   <div id="search">
-  <form id="form" action = "index.php">
-    Enter an address: <input id="address" type="text" name="address" /><br />
-    <input id="submit" type="button" value="Locate" onclick="codeAddress()"/>
+  <form id="form">
+    <input placeholder="Enter your address here" id="address" type="text" name="address"/>
+    <input id="submit" type="image" src="images/button_locate.png" onclick="codeAddress()"/>
   </form> 
   </div>
 
